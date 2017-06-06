97ed74ad95ccb6c7899b2c85beea0879a7f7f3ea
yibin <961878139@qq.com>
2017-06-06 16:18:01 +0800

    更新股票接口，使用量亿数据

--- galaxy-moon/src/main/java/use/forecast/stock/ForecastStock.java
+++ galaxy-moon/src/main/java/use/forecast/stock/ForecastStock.java
@@ -1,26 +1,32 @@
 package use.forecast.stock;
 
 import api.Processor;
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.JsonUtils;
 import utils.Utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yibin on 16/3/30.
  */
 public class ForecastStock {
     private static final Logger logger = LoggerFactory.getLogger(ForecastStock.class);
-    private static List<Integer> ranges = Lists.newArrayList(3, 5, 7, 10, 15);
-    private static List<Integer> begins = Lists.newArrayList(30,56,73,90,120);
 
     public static void main(String[] args) throws Exception{
 //        buyAndSell();
-        dingtou();
+        dingtou("000001","2010-06-05","2014-06-05");
+//        dingtou("000002","2014-06-05","2017-06-05");
+//        dingtou("000025","2014-06-05","2017-06-05");
     }
 
     /**
@@ -31,9 +37,65 @@ public class ForecastStock {
 
     }
 
-    private static void dingtou() throws Exception{
-        List<StockInfo> stockInfoList = FetchService.fetchHistoryData("000001","2017-06-01","2017-06-05");
-        System.out.println(JsonUtils.serialize(stockInfoList));
+    private static void dingtou(String code, String start, String end) throws Exception{
+        List<StockInfo> stockInfoList = FetchService.fetchHistoryData(code,start,end);
+        if(stockInfoList == null || stockInfoList.size() == 0){
+            System.out.println("no data");
+            return;
+        }
+
+        List<Map<String,Object>> result = Lists.newArrayList();
+        for(int zhouqi = 1; zhouqi < 31;zhouqi++){
+            for(int sellpoint = 1; sellpoint < 100;sellpoint++){
+                double totalin = 0;
+                double totalout = 0;
+                double times = 0;
+                int hold = 0;
+                double holdvalue = 0;
+                for(int day =0;day<stockInfoList.size();day++){
+                    if(stockInfoList.get(day).getClosePrice() * hold - holdvalue > holdvalue * sellpoint / 100){//超过卖点
+                        totalin += stockInfoList.get(day).getClosePrice() * hold;
+                        times ++;
+                        hold = 0;
+                        holdvalue = 0;
+                    }
+                    if(day % zhouqi == 0){//买入
+                        totalout += stockInfoList.get(day).getClosePrice();
+                        hold ++;
+                        holdvalue += stockInfoList.get(day).getClosePrice();
+                    }
+                }
+                Map<String,Object> map = Maps.newHashMap();
+                map.put("totalin",totalin);
+                map.put("totalout",totalout);
+                map.put("times",times);
+                map.put("hold",hold);
+                map.put("holdvalue",holdvalue);
+                map.put("zhouqi",zhouqi);
+                map.put("sellpoint",sellpoint);
+                result.add(map);
+            }
+        }
+
+        BufferedWriter writer = Files.newWriter(new File("D:\\projects\\mine\\"+code+"-"+start+end+".csv"), Charsets.UTF_8);
+        Joiner j = Joiner.on(",");
+        for(Map<String,Object> map : result){
+            String s = j.join(map.get("totalin")
+                    , map.get("totalout")
+                    , map.get("times")
+                    , map.get("hold")
+                    , map.get("holdvalue")
+                    , map.get("zhouqi")
+                    , map.get("sellpoint")
+                    , (Double)map.get("totalin") + (Double) map.get("holdvalue") - (Double)map.get("totalout")
+                    , (Double)map.get("totalout") > 0 ? ((Double)map.get("totalin") + (Double) map.get("holdvalue")) / (Double)map.get("totalout") : 0
+            );
+            writer.write(s);
+            writer.newLine();
+        }
+        writer.close();
+
+        System.out.println(code);
     }
 
 //    public static void forecast(String code, String market) {
