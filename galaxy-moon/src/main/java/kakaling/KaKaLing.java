f12c6a7fbe514bd716c9ce2640a872488e7f58bc
yibin <bin.yi@qunar.com>
2017-07-16 11:11:38 +0800

    kakaling

---
+++ galaxy-moon/src/main/java/kakaling/KaKaLing.java
@@ -0,0 +1,10 @@
+package kakaling;
+
+/**
+ * Created by yibin on 17/7/16.
+ */
+public class KaKaLing {
+    public static void main(String[] args) {
+        System.out.println("hello kakaling");
+    }
+}
